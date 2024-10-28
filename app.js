32544cc7007701a999b465e46b41974e1982cf66
worsleyj <josephworsley23@gmail.com>
2024-10-28 14:59:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("hello, world!"));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("My first express app ~ listening on port ${PORT}!");
+});
