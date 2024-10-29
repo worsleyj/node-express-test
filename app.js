0abda0b123ae292c25ed2e96d7d78d60f37f0940
worsleyj <josephworsley23@gmail.com>
2024-10-29 14:30:36 -0400

    link routers up to main

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const app = express();
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
 
-app.get("/", (req, res) => res.send("hello, world!"));
-app.get("/:username/messages", (req, res) => {
-  console.log(req.params);
-  console.log("Query:", req.query);
-  res.end();
-});
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/*", indexRouter);
 
-app.get("/:username/messages/:messageId", (req, res) => {
-  console.log(req.params);
-  res.end();
-});
+// app.get("/", (req, res) => res.send("hello, world!"));
+// app.get("/:username/messages", (req, res) => {
+//   console.log(req.params);
+//   console.log("Query:", req.query);
+//   res.end();
+// });
+
+// app.get("/:username/messages/:messageId", (req, res) => {
+//   console.log(req.params);
+//   res.end();
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
