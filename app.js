288a9c77ab21c826ac58cb24d1be9b815f2e79fe
worsleyj <josephworsley23@gmail.com>
2024-10-29 14:31:04 -0400

    remove .get calls, move port declaration

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+const PORT = process.env.PORT || 3000;
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/*", indexRouter);
 
-// app.get("/", (req, res) => res.send("hello, world!"));
-// app.get("/:username/messages", (req, res) => {
-//   console.log(req.params);
-//   console.log("Query:", req.query);
-//   res.end();
-// });
-
-// app.get("/:username/messages/:messageId", (req, res) => {
-//   console.log(req.params);
-//   res.end();
-// });
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`My first express app ~ listening on port ${PORT}!`);
 });
