86baa91ac048634118ad9951f5e142b39c30cd2e
worsleyj <josephworsley23@gmail.com>
2024-10-29 14:20:15 -0400

    add author router

---
+++ routes/authorRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const authorRouter = Router();
+
+authorRouter.get("/", (req, res) => res.send("All authors"));
+authorRouter.get("/:authorId", (req, res) => {
+  const { authorId } = req.params;
+  res.send(`Author ID: ${authorId}`);
+});
+
+module.exports = authorRouter;
