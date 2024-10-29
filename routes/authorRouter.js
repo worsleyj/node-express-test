ba3e7cb2976f535f5f28355e00793f023b2b778e
worsleyj <josephworsley23@gmail.com>
2024-10-29 15:11:36 -0400

    link authorRouter and authorController

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
+const { getAuthorById } = require("../controllers/authorController");
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
-
+authorRouter.get("/:authorId", getAuthorById);
+// authorRouter.get("/:authorId", (req, res) => {
+//   const { authorId } = req.params;
+//   res.send(`Author ID: ${authorId}`);
+// });
 module.exports = authorRouter;
