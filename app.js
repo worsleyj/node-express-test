320439939a097eff099d3a128af6039a69bfecec
worsleyj <josephworsley23@gmail.com>
2024-10-29 14:04:23 -0400

    add routing params

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("hello, world!"));
+app.get("/:username/messages", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
+
+app.get("/:username/messages/:messageId", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
