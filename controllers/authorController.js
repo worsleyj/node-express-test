44bdf569086430b013d9277b6c80b240dc4f72eb
worsleyj <josephworsley23@gmail.com>
2024-10-29 14:44:52 -0400

    add controllers and db.js

---
+++ controllers/authorController.js
@@ -0,0 +1,13 @@
+const db = require("../db");
+
+async function getAuthorById(req, res) {
+  const { authorId } = req.params;
+  const author = await db.getAuthorById(Number(authorId));
+  if (!author) {
+    res.status(404).send("Author not Found");
+    return;
+  }
+  res.send(`Author Name: ${author.name}`);
+}
+
+module.exports = { getAuthorById };
