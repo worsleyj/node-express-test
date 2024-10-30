6abf47e9a0461df2762915d235b55cd11cc86c0c
worsleyj <josephworsley23@gmail.com>
2024-10-30 14:52:00 -0400

    remove commented out code

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -7,8 +7,6 @@ const getAuthorById = asyncHandler(async (req, res) => {
   const author = await db.getAuthorById(Number(authorId));
   if (!author) {
     throw new CustomNotFoundError("Author not Found");
-    // res.status(404).send("Author not Found");
-    // return;
   }
   res.send(`Author Name: ${author.name}`);
 });
