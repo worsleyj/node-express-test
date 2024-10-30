f22ade44ecdd0022cc4d59059a6baa8622d82212
worsleyj <josephworsley23@gmail.com>
2024-10-30 15:09:54 -0400

    add index.ejs

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Index"));
+indexRouter.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
 
 module.exports = indexRouter;
